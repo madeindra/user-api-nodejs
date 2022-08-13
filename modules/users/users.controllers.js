f0d7a07d39a6b0c6b05f5a5460dbb49a9d3fc793
Made Indra <made.indra@protonmail.com>
2022-08-13 09:10:24 +0700

    fix: refactor controller to use response sender

--- modules/users/users.controllers.js
+++ modules/users/users.controllers.js
@@ -1,64 +1,37 @@
 // import dependencies
 const service = require('./users.services');
-const validation = require('./user.validations');
+const response = require('../../libs/response');
 
 async function register(req, res) {
   // deconstruct body
   const { body } = req;
 
-  // validate body
-  const { error, value } = validation.register.validate(body);
-  if (error) {
-    return res.status(400).json({
-      success: false,
-      message: error?.details[0]?.message || 'Bad request.',
-    });
-  }
-
   // pass to service
   let result
   try {
-    result = await service.register(value);
+    result = await service.register(body);
   } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.toString(),
-    });
+    return response.send(res, err);
   }
 
   // return result
-  return res.status(200).json({
-    success: true,
-    message: 'Registration successful.'
-  });
+  return response.send(res, result);
 }
 
 async function login(req, res) {
   // deconstruct body
   const { body } = req;
 
-  // validate body
-  const { error, value } = validation.login.validate(body);
-  if (error) {
-    return res.status(400).json({
-      success: false,
-      message: error?.details[0]?.message || 'Bad request.',
-    });
-  }
-
   // pass to service
   let result
   try {
-    result = await service.login(value);
+    result = await service.login(body);
   } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.toString(),
-    });
+    return response.send(res, err);
   }
 
   // return result
-  return res.status(200).json(result);
+  return response.send(res, result);
 }
 
 module.exports = {
