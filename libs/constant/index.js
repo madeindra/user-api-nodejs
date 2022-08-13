b2ee8c469b60cf112baf030dbb87883e427ce03e
Made Indra <made.indra@protonmail.com>
2022-08-13 09:09:57 +0700

    feat: add http code & message constant

--- libs/constant/index.js
+++ libs/constant/index.js
@@ -9,7 +9,34 @@ const ROLES = Object.freeze({
   ADMIN: 'admin',
 });
 
+// status codes
+const CODE = Object.freeze({
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+});
+
+// response message
+const MESSAGE = Object.freeze({
+  GENERAL: 'Operation successful.',
+  BAD_REQUEST: 'Bad request.',
+  DATABASE_FAILED: 'Database operation failed.',
+  EMAIL_INVALID: 'Invalid email format.',
+  CREDENTIAL_INVALID: 'Invalid email or password',
+  EMAIL_REGISTERED: 'Email already registered.',
+  REGISTRATION_SUCCESS: 'Registration successful.',
+  LOGIN_SUCCESS: 'Login successful.',
+});
+
 module.exports = {
   COLLECTION,
   ROLES,
+  CODE,
+  MESSAGE,
 }
