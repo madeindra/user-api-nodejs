d2ae22dae09d073cfeaea44505caaec5efb54491
Made Indra <made.indra@protonmail.com>
2022-08-13 12:16:57 +0700

    fix: prefix unused vars with _

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,6 @@
 // error middleware
 // eslint-disable-next-line no-unused-vars
-function errorMiddleware(err, req, res, next) {
+function errorMiddleware(_err, _req, res, _next) {
   return res.status(500).json({
     success: false,
     message: 'An error occured.',
--- middlewares/notfound.js
+++ middlewares/notfound.js
@@ -1,6 +1,6 @@
 // not found middleware
 // eslint-disable-next-line no-unused-vars
-function notfoundMiddleware(req, res, next) {
+function notfoundMiddleware(_req, res, _next) {
   return res.status(404).json({
     success: false,
     message: 'Route not found.',
