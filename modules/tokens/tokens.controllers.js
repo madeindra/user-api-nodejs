97331f476f735964e835b07f22798a7696586d11
Made Indra <made.indra@protonmail.com>
2022-08-14 18:36:32 +0700

    fix: pass user data to controller on refresh

--- modules/tokens/tokens.controllers.js
+++ modules/tokens/tokens.controllers.js
@@ -3,13 +3,13 @@ const service = require('./tokens.services');
 const response = require('../../libs/response');
 
 async function refresh(req, res) {
-  // deconstruct body
-  const { body } = req;
+  // deconstruct body & user from token
+  const { body, user } = req;
 
   // pass to service
   let result;
   try {
-    result = await service.refresh(body);
+    result = await service.refresh(body, user);
   } catch (err) {
     return response.send(res, err);
   }
