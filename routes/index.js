a0ad88c7e31ebee943927b662f006bc96e198d38
Made Indra <made.indra@protonmail.com>
2022-08-14 18:10:54 +0700

    fix: map refresh route to token controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 // import controllers
 const homeController = require('../modules/home/home.controller');
 const usersController = require('../modules/users/users.controllers');
+const tokensController = require('../modules/tokens/tokens.controllers');
 
 // main route
 router.get('/', homeController.ping);
@@ -11,7 +12,7 @@ router.get('/', homeController.ping);
 // users route
 router.post('/api/v1/register', usersController.register);
 router.post('/api/v1/login', usersController.login);
-router.get('/api/v1/refresh', usersController.refresh);
+router.get('/api/v1/refresh', tokensController.refresh);
 
 // export router
 module.exports = router;
