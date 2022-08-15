b16b27632d241667e61fd68d12de938209c04251
Made Indra <made.indra@protonmail.com>
2022-08-15 08:03:20 +0700

    fix: update routes to use plural noun

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,11 @@ router.post('/api/v1/auth/login', usersController.login);
 router.post('/api/v1/auth/refresh', tokensController.refresh);
 
 // user route (authenticated admin only)
-router.post('/api/v1/user', bearerMiddleware, adminOnly, usersController.createUser);
-router.get('/api/v1/user', bearerMiddleware, adminOnly, usersController.readAllUsers);
-router.get('/api/v1/user/:id', bearerMiddleware, adminOnly, usersController.readOneUser);
-router.put('/api/v1/user/:id', bearerMiddleware, adminOnly, usersController.updateUser);
-router.delete('/api/v1/user/:id', bearerMiddleware, adminOnly, usersController.deleteUser);
+router.post('/api/v1/users', bearerMiddleware, adminOnly, usersController.createUser);
+router.get('/api/v1/users', bearerMiddleware, adminOnly, usersController.readAllUsers);
+router.get('/api/v1/users/:id', bearerMiddleware, adminOnly, usersController.readOneUser);
+router.put('/api/v1/users/:id', bearerMiddleware, adminOnly, usersController.updateUser);
+router.delete('/api/v1/users/:id', bearerMiddleware, adminOnly, usersController.deleteUser);
 
 // profile route (authenticated only)
 router.get('/api/v1/profile', bearerMiddleware, usersController.getProfile);
