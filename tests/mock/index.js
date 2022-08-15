8923f2d0cfe8217ca2fe0511694a9ffb2f3bc86c
Made Indra <made.indra@protonmail.com>
2022-08-15 10:54:14 +0700

    test: add request, response, next mocker

---
+++ tests/mock/index.js
@@ -0,0 +1,31 @@
+// mock request with custom data
+function mockRequest(body, header, params, query){
+  return {
+    body,
+    header,
+    params,
+    query
+  }
+}
+
+// mock response with jest because it will be checked using expect
+function mockResponse() {
+  const res = {};
+
+  res.send = jest.fn().mockReturnValue(res);
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+
+  return res;
+}
+
+// mock next with jest
+function mockNext() {
+  return jest.fn();
+} 
+
+module.exports = {
+  mockRequest,
+  mockResponse,
+  mockNext,
+}
